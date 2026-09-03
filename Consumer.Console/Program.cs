1199687ae378caf18e4d709563938194dc897d15
Ayham Alaama <dotnet_dev@mail.ru>
2026-09-03 19:20:48 +0300

    add service collection to console app and fix projects names

---
+++ Consumer.Console/Program.cs
@@ -0,0 +1,53 @@
+﻿
+
+using Grpc.Net.Client;
+using Microsoft.Extensions.DependencyInjection;
+
+var channel = GrpcChannel.ForAddress("http://localhost:5259");
+
+var customerClient = new GrpcCon.Customer.CustomerClient(channel);
+
+var createCustomerRequest = new GrpcCon.CreateCustomerRequest
+{
+    Name = "Ahmad",
+    Age = 30
+};
+
+var createCustomerReply = await customerClient.CreateAsync(createCustomerRequest);
+
+Console.WriteLine(createCustomerReply.Message);
+
+Console.ReadKey();
+
+// ================================
+
+var services = new ServiceCollection();
+
+
+services.AddGrpcClient<GrpcCon.Customer.CustomerClient>(o =>
+{
+    o.Address = new Uri("http://localhost:5259");
+});
+
+var serviceProvider = services.BuildServiceProvider();
+
+var customerClientFromDI = serviceProvider.GetRequiredService<GrpcCon.Customer.CustomerClient>();
+
+var createCustomerRequestFromDI = new GrpcCon.CreateCustomerRequest
+{
+    Name = "Ahmad",
+    Age = 30
+};
+
+var createCustomerReplyFromDI = await customerClientFromDI.CreateAsync(createCustomerRequestFromDI);
+
+async static void CreateCustomer(GrpcCon.Customer.CustomerClient client)
+{
+    var createCustomerRequestFromDI = new GrpcCon.CreateCustomerRequest
+    {
+        Name = "Ahmad",
+        Age = 30
+    };
+
+    var createCustomerReplyFromDI = await client.CreateAsync(createCustomerRequestFromDI);
+}
